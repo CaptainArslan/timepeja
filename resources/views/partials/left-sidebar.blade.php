d0072ef7ecc0b226b03be89dd26ef5bcff24e65a
CaptainArslan <mughalarslan996@gmail.com>
2023-01-07 20:48:45 +0500

    project intial commit

---
+++ resources/views/partials/left-sidebar.blade.php
@@ -0,0 +1,305 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="left-side-menu">
+
+    <div class="h-100" data-simplebar>
+
+        <!-- User box -->
+        <div class="user-box text-center">
+            <img src="{{asset('images/users/user-1.jpg')}}" alt="user-img" title="Mat Helme" class="rounded-circle avatar-md">
+            <div class="dropdown">
+                <a href="javascript: void(0);" class="text-dark dropdown-toggle h5 mt-2 mb-1 d-block" data-bs-toggle="dropdown">Geneva Kennedy</a>
+                <div class="dropdown-menu user-pro-dropdown">
+
+                    <!-- item-->
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i class="fe-user me-1"></i>
+                        <span>My Account</span>
+                    </a>
+
+                    <!-- item-->
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i class="fe-settings me-1"></i>
+                        <span>Settings</span>
+                    </a>
+
+                    <!-- item-->
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i class="fe-lock me-1"></i>
+                        <span>Lock Screen</span>
+                    </a>
+
+                    <!-- item-->
+                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                        <i class="fe-log-out me-1"></i>
+                        <span>Logout</span>
+                    </a>
+
+                </div>
+            </div>
+            <p class="text-muted">Admin Head</p>
+        </div>
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+
+            <ul id="side-menu">
+
+                <!-- <li class="menu-title">Navigation</li> -->
+
+                <li>
+                    <a href="#sidebarDashboards" data-bs-toggle="collapse">
+                        <i class="mdi mdi-view-dashboard-outline"></i>
+                        <!-- <span class="badge bg-info rounded-pill float-end">4</span> -->
+                        <span> Dashboards </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="apps-calendar.html">
+                        <i class="fas fa-user-alt"></i>
+                        <span> Manager </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="apps-calendar.html">
+                        <i class=" fas fa-car-alt"></i>
+                        <span> Vehicles </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="apps-calendar.html">
+                        <i class="fas fa-user"></i>
+                        <span> Driver </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="apps-calendar.html">
+                        <i class="fas fa-users"></i>
+                        <span> Passenger </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="apps-calendar.html">
+                        <i class="fas fa-route"></i>
+                        <span> Route </span>
+                    </a>
+                </li>
+                <!-- <li class="menu-title mt-2">Manager</li> -->
+<!-- 
+                <li>
+                    <a href="apps-calendar.html">
+                        <i class="mdi mdi-calendar"></i>
+                        <span> Calendar </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="apps-chat.html">
+                        <i class="mdi mdi-forum-outline"></i>
+                        <span> Chat </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#sidebarEcommerce" data-bs-toggle="collapse">
+                        <i class="mdi mdi-cart-outline"></i>
+                        <span> Ecommerce </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarEcommerce">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="ecommerce-dashboard.html">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-products.html">Products</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-product-detail.html">Product Detail</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-product-edit.html">Add Product</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-customers.html">Customers</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-orders.html">Orders</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-order-detail.html">Order Detail</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-sellers.html">Sellers</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-cart.html">Shopping Cart</a>
+                            </li>
+                            <li>
+                                <a href="ecommerce-checkout.html">Checkout</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#sidebarCrm" data-bs-toggle="collapse">
+                        <i class="mdi mdi-account-multiple-outline"></i>
+                        <span> CRM </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarCrm">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="crm-dashboard.html">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="crm-contacts.html">Contacts</a>
+                            </li>
+                            <li>
+                                <a href="crm-opportunities.html">Opportunities</a>
+                            </li>
+                            <li>
+                                <a href="crm-leads.html">Leads</a>
+                            </li>
+                            <li>
+                                <a href="crm-customers.html">Customers</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#sidebarEmail" data-bs-toggle="collapse">
+                        <i class="mdi mdi-email-multiple-outline"></i>
+                        <span> Email </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarEmail">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="email-inbox.html">Inbox</a>
+                            </li>
+                            <li>
+                                <a href="email-read.html">Read Email</a>
+                            </li>
+                            <li>
+                                <a href="email-compose.html">Compose Email</a>
+                            </li>
+                            <li>
+                                <a href="email-templates.html">Email Templates</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="apps-social-feed.html">
+                        <span class="badge bg-primary float-end">Hot</span>
+                        <i class="mdi mdi-rss"></i>
+                        <span> Social Feed </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="apps-companies.html">
+                        <i class="mdi mdi-domain"></i>
+                        <span> Companies </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#sidebarProjects" data-bs-toggle="collapse">
+                        <i class="mdi mdi-briefcase-check-outline"></i>
+                        <span> Projects </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarProjects">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="project-list.html">List</a>
+                            </li>
+                            <li>
+                                <a href="project-detail.html">Detail</a>
+                            </li>
+                            <li>
+                                <a href="project-create.html">Create Project</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#sidebarTasks" data-bs-toggle="collapse">
+                        <i class="mdi mdi-clipboard-multiple-outline"></i>
+                        <span> Tasks </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarTasks">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="task-list.html">List</a>
+                            </li>
+                            <li>
+                                <a href="task-details.html">Details</a>
+                            </li>
+                            <li>
+                                <a href="task-kanban-board.html">Kanban Board</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#sidebarContacts" data-bs-toggle="collapse">
+                        <i class="mdi mdi-book-account-outline"></i>
+                        <span> Contacts </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarContacts">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="contacts-list.html">Members List</a>
+                            </li>
+                            <li>
+                                <a href="contacts-profile.html">Profile</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#sidebarTickets" data-bs-toggle="collapse">
+                        <i class="mdi mdi-lifebuoy"></i>
+                        <span> Tickets </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarTickets">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="tickets-list.html">List</a>
+                            </li>
+                            <li>
+                                <a href="tickets-detail.html">Detail</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="apps-file-manager.html">
+                        <i class="mdi mdi-folder-star-outline"></i>
+                        <span> File Manager </span>
+                    </a>
+                </li> -->
+            </ul>
+
+        </div>
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
+<!-- Left Sidebar End -->
