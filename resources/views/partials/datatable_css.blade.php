ad78546e0260e400c213efc62e806cb73adca7f8
CaptainArslan <87141902+CaptainArslan@users.noreply.github.com>
2023-03-16 23:58:22 +0500

    Remove all code from branch

--- resources/views/partials/datatable_css.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- third party css -->
-<link href="/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-<link href="/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-<link href="/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-<link href="/libs/datatables.net-select-bs5/css//select.bootstrap5.min.css" rel="stylesheet" type="text/css" />
-<!-- third party css end -->
