67aacc915b283247d56689b86b8146f7bd4e468c
CaptainArslan <87141902+CaptainArslan@users.noreply.github.com>
2023-04-18 05:26:25 +0500

    create custom command for to add schedule for today

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('schedule:seed')->daily();
+        $schedule->command('schedule:seed')->daily();
         // $schedule->command('inspire')->hourly();
     }
 
