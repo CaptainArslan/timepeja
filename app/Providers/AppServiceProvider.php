4a0e191e775a3c0391bae7ad0e7d54d97b5501b1
CaptainArslan <87141902+CaptainArslan@users.noreply.github.com>
2023-04-29 00:04:18 +0500

    filter telescope only for local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
+use Laravel\Telescope\Telescope;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Telescope\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,10 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // if ($this->app->environment('local')) {
-        //     $this->app->register(TelescopeServiceProvider::class);
-        //     $this->app->register(TelescopeServiceProvider::class);
-        // }
+        Telescope::filter(function ($request) {
+            return app()->environment('local') && $request->ip() === '127.0.0.1';
+        });
     }
 
     /**
