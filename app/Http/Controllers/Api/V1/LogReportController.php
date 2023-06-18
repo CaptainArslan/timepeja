ee6cae81592ed6574ee1f5c9cf6ce03c174ec1f6
CaptainArslan <mughalarslan996@gmail.com>
2023-06-18 00:39:52 -0700

    update log report api for to print pdf in a single api

--- app/Http/Controllers/Api/V1/LogReportController.php
+++ app/Http/Controllers/Api/V1/LogReportController.php
@@ -66,9 +66,8 @@ class LogReportController extends BaseController
 
             $manager = auth('manager')->user();
 
-
             $result = $query->where('o_id', $manager->o_id)
-                // ->where('status', Schedule::STATUS_PUBLISHED)
+                ->where('status', Schedule::STATUS_PUBLISHED)
                 ->with('organizations:id,name,branch_name,branch_code,email,phone,address,code')
                 ->with('routes:id,name,number,from,to')
                 ->with('vehicles:id,number')
@@ -76,7 +75,8 @@ class LogReportController extends BaseController
                 ->select('id', 'o_id', 'route_id', 'v_id', 'd_id', 'date', 'time as scheduled_time', 'start_time', 'end_time', 'is_delay', 'trip_status', 'delayed_reason')
                 ->orderby('trip_status', 'desc')
                 ->get();
-            $download_url = 'www.google.com';
+
+            $download_url = '';
 
             if ($result->isNotEmpty()) {
                 $download_url = $this->creatdPdf($request, $result);
@@ -87,72 +87,6 @@ class LogReportController extends BaseController
         } catch (\Throwable $th) {
             throw $th;
         }
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     public function creatdPdf(Request $request, $data)
@@ -174,13 +108,15 @@ class LogReportController extends BaseController
         $pdfModel->url = asset('/uploads/pdf/' . $filename);
 
         if ($pdfModel->save()) {
-            return $this->respondWithSuccess($pdfModel, 'Pdf Created Successfully', 'LOG_REPORT_PDF_CREATED_SUCCESSFULLY');
+            return $pdfModel->url;
+            // return $this->respondWithSuccess($pdfModel, 'Pdf Created Successfully', 'LOG_REPORT_PDF_CREATED_SUCCESSFULLY');
         } else {
             // Delete the saved PDF file if model saving failed
             if (file_exists($filePath)) {
                 unlink($filePath);
             }
-            return $this->respondWithError('Error occurred while creating the PDF. Failed to save the model.');
+            return null;
+            // return $this->respondWithError('Error occurred while creating the PDF. Failed to save the model.');
         }
     }
 }
