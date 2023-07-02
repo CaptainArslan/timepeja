f5a4d493e5cf140e3ea9a341df878a49075d1a88
CaptainArslan <mughalarslan996@gmail.com>
2023-07-02 11:39:35 -0700

    update request migration, and add foreign keys to the student, employees, and guardians

--- app/Models/Requests.php
+++ app/Models/Requests.php
@@ -19,6 +19,11 @@ class Requests extends Model
     public const REQUEST_STATUS_EMPLOYEE = 'employee';
     public const REQUEST_STATUS_GUARDIAN = 'guardian';
 
+    public const REQUEST_STATUS_PENDING = 'pending';
+    public const REQUEST_STATUS_APPROVE = 'approve';
+    public const REQUEST_STATUS_DISAPPROVE = 'disapprove';
+    public const REQUEST_STATUS_MEET_PEROSONALLY = 'meet-personally';
+
     public const STUDENT_SCHOOL = 'scchool';
     public const STUDENT_COLLEGE = 'college';
     public const STUDENT_UNIVERSITY = 'university';
@@ -33,7 +38,8 @@ class Requests extends Model
      */
     public function route()
     {
-        return $this->belongsTo(Route::class, 'r_id', 'id');
+        // return $this->belongsTo(Route::class, 'r_id', 'id');
+        return $this->belongsTo(Route::class);
     }
 
     /**
@@ -43,7 +49,8 @@ class Requests extends Model
      */
     public function passenger()
     {
-        return $this->belongsTo(Passenger::class, 'p_id', 'id');
+        // return $this->belongsTo(Passenger::class, 'p_id', 'id');
+        return $this->belongsTo(Passenger::class);
     }
 
 
@@ -54,6 +61,7 @@ class Requests extends Model
      */
     public function organization()
     {
-        return $this->belongsTo(Organization::class, 'o_id', 'id');
+        // return $this->belongsTo(Organization::class, 'o_id', 'id');
+        return $this->belongsTo(Organization::class);
     }
 }
--- database/migrations/2023_06_18_152626_create_requests_table.php
+++ database/migrations/2023_06_18_152626_create_requests_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Requests;
-use Faker\Core\Uuid;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,49 +17,58 @@ class CreateRequestsTable extends Migration
         Schema::create('requests', function (Blueprint $table) {
             $table->id();
             $table->foreignId('passenger_id')->nullable()->constrained('passengers')->onDelete('cascade');
-            $table->foreignId('o_id')->constrained('organizations')->onDelete('cascade');
-            $table->string('name')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('email')->nullable();
-            $table->string('house_no')->nullable();
-            $table->string('street_no')->nullable();
-            $table->string('town')->nullable();
-            $table->string('additional_detail')->nullable();
-            $table->foreignId('city_id')->constrained('cities');
-            $table->string('pickup_address')->nullable();
-            $table->foreignId('pickup_city_id')->constrained('cities');
-            $table->string('lattitude')->nullable();
-            $table->string('longitude')->nullable();
-            $table->enum('type', [
-                Requests::REQUEST_STATUS_STUDENT,
-                Requests::REQUEST_STATUS_EMPLOYEE,
-                Requests::REQUEST_STATUS_GUARDIAN,
-            ]);
-            $table->enum('sub_type', [
-                Requests::STUDENT_SCHOOL,
-                Requests::STUDENT_COLLEGE,
-                Requests::STUDENT_UNIVERSITY,
-                Requests::EMPLOYEE,
-                Requests::GUARDIAN,
-            ])->nullable();
+            $table->foreignId('organization_id')->constrained('organizations')->onDelete('cascade');
             $table->string('roll_no')->nullable();
             $table->string('class')->nullable();
             $table->string('section')->nullable();
             $table->longText('profile_card')->nullable();
             $table->string('descipline')->nullable();
             $table->string('designation')->nullable();
-            $table->string('empolyee_id')->nullable();
-            $table->foreignId('r_id')->nullable()->constrained('routes')->onDelete('cascade');
+            $table->string('empolyee_comp_id')->nullable();
+            $table->foreignId('route_id')->nullable()->constrained('routes')->onDelete('cascade');
             $table->date('transport_start_time')->nullable();
             $table->date('transport_end_time')->nullable();
             $table->string('qualification')->nullable();
-            $table->integer('btach_year')->nullable();
+            $table->integer('batch_year')->nullable();
             $table->integer('degree_duration')->nullable();
-            $table->string('cnic_no')->nullable();
-            $table->string('cnic_front_image')->nullable();
-            $table->string('cnic_back_image')->nullable();
-            $table->string('relation')->nullable();
-            $table->string('guardian_code')->nullable();
+            $table->enum('type', [
+                Requests::REQUEST_STATUS_STUDENT,
+                Requests::REQUEST_STATUS_EMPLOYEE,
+                Requests::REQUEST_STATUS_GUARDIAN,
+            ]);
+
+            $table->enum('request_status', [
+                Requests::REQUEST_STATUS_PENDING,
+                Requests::REQUEST_STATUS_APPROVE,
+                Requests::REQUEST_STATUS_DISAPPROVE,
+                Requests::REQUEST_STATUS_MEET_PEROSONALLY,
+            ]);
+            // $table->string('name')->nullable();
+            // $table->string('phone')->nullable();
+            // $table->string('email')->nullable();
+            // $table->string('house_no')->nullable();
+            // $table->string('street_no')->nullable();
+            // $table->string('town')->nullable();
+            // $table->string('additional_detail')->nullable();
+            // $table->foreignId('city_id')->constrained('cities');
+            // $table->string('pickup_address')->nullable();
+            // $table->foreignId('pickup_city_id')->constrained('cities');
+            // $table->string('lattitude')->nullable();
+            // $table->string('longitude')->nullable();
+            
+            // $table->enum('sub_type', [
+            //     Requests::STUDENT_SCHOOL,
+            //     Requests::STUDENT_COLLEGE,
+            //     Requests::STUDENT_UNIVERSITY,
+            //     Requests::EMPLOYEE,
+            //     Requests::GUARDIAN,
+            // ])->nullable();
+            
+            // $table->string('cnic_no')->nullable();
+            // $table->string('cnic_front_image')->nullable();
+            // $table->string('cnic_back_image')->nullable();
+            // $table->string('relation')->nullable();
+            // $table->string('guardian_code')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
