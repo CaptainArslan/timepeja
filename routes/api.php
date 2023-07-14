2961d286b38e746769aa59a35afb28dec800b8f3
CaptainArslan <mughalarslan996@gmail.com>
2023-07-14 15:36:12 -0700

    update driver print pdf api

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,7 @@ use App\Http\Controllers\Api\V1\PassengerRequestController;
 Route::group(['middleware' => 'api'], function () {
     Route::prefix('v1/manager')->name('manager.')->group(function () {
 
-        Route::get('/drivers/pdf', [ApiDriverController::class, 'createPdf']);
+
         // Manager Auth
         Route::post('/register', [ManagerAuthController::class, 'register']);
         Route::post('/login', [ManagerAuthController::class, 'login']);
@@ -81,9 +81,10 @@ Route::group(['middleware' => 'api'], function () {
 
             // Driver Api
             Route::resource('/driver', ApiDriverController::class);
-            
-
+            Route::get('/drivers/pdf', [ApiDriverController::class, 'createPdf']);
             Route::get('/search/driver', [ApiDriverController::class, 'search']);
+
+
             // Driver api for web
             Route::group(['prefix' => 'web/driver'], function () {
                 Route::get('/', [ApiDriverController::class, 'getDriver']);
