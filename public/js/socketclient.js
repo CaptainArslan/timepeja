489aa43d89252241ca2aac91cd9b308a48d9eb59
Muhammad Arslan <mughalarslan996@gmail.com>
2024-08-19 01:27:22 +0500

    update logs on server client for testing

--- public/js/socketclient.js
+++ public/js/socketclient.js
@@ -3,8 +3,8 @@ const domain = origin.splice(0, 2).join(":");
 const port = 3000;
 const ip = domain + ":" + port;
 
-// const socket = io("https://socket-testing.stoppick.com", {
-const socket = io("http://localhost:3000", {
+// const socket = io("http://localhost:3000", {
+const socket = io("https://socket-testing.stoppick.com", {
     transports: ["polling", "websocket"],
 });
 
